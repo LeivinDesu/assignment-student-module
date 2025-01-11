7ef97ce57b766f6abd302e9957fabe9b50582fb4
Christian Oliver Santarin <santarinchristian5@gmail.com>
2025-01-11 20:02:40 +0800

    Databse connection and Student Entity

---
+++ src/entity/student.entity.ts
@@ -0,0 +1,26 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity('Student')
+
+export class Student{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    fistname: string;
+
+    @Column()
+    lastname: string;
+
+    @Column({unique: true})
+    email: string;
+
+    @Column()
+    enrollmentDate: Date;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
